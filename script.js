3c82858b50bd7082e7cf80057776558da7357193
mgs786 <muhammad.salman@stonybrook.edu>
2026-04-16 15:02:29 -0400

    Add simple fade-in animation on scroll

---
+++ script.js
@@ -0,0 +1,14 @@
+// simple fade-in animation
+const sections = document.querySelectorAll('.section');
+
+window.addEventListener('scroll', () => {
+  sections.forEach(sec => {
+    const top = window.scrollY;
+    const offset = sec.offsetTop - 300;
+
+    if (top > offset) {
+      sec.style.opacity = 1;
+      sec.style.transform = "translateY(0)";
+    }
+  });
+});
