85f5dc88876d4d9c8b229b8ea0aae618ee0156f1
Muhammad G Salman <muhammad.salman@gmail.com>
2026-04-16 16:45:04 -0400

    updated portfolio with image and full content

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
-  });
-});
+function scrollTopPage() {
+  window.scrollTo({top:0, behavior:"smooth"});
+}
