f7029b27ef57c9464fbca591992311fbd1869249
mgs786 <muhammad.salman@stonybrook.edu>
2026-04-16 15:51:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-// simple fade-in animation
-const sections = document.querySelectorAll('.section');
+const sections = document.querySelectorAll('.fade');
 
 window.addEventListener('scroll', () => {
   sections.forEach(sec => {
     const top = window.scrollY;
-    const offset = sec.offsetTop - 300;
+    const offset = sec.offsetTop - 400;
 
     if (top > offset) {
-      sec.style.opacity = 1;
-      sec.style.transform = "translateY(0)";
+      sec.classList.add('show');
     }
   });
 });
