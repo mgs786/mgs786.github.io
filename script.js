ecf10ca3296f789763acbe455591cd5a801d3945
Muhammad G Salman <muhammad.salman@gmail.com>
2026-04-16 17:27:43 -0400

    updated portfolio with image and full content

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function scrollTopPage() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
+
+/* APPLE SCROLL EFFECT */
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
